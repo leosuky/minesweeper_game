929d62e6f69de911066d53ea264059028c8d18d4
Sunkanmi-neulogic <sunkanmi.samson@neulogicsolutions.com>
2023-05-15 12:24:16 +0100

    completed basic game logic

---
+++ src/helpers/Field.test.ts
@@ -0,0 +1,98 @@
+import { emptyFieldGenerator, fieldGenerator, CellState, Cell } from "./Field";
+
+const { empty, bomb, hidden} = CellState;
+
+const cellWithBombFilter = (cell: Cell) => cell === bomb;
+
+describe('Field Generator', () => {
+    describe('emptyFieldGenerator tests', () => {
+        it('2x2', () => {
+            expect(emptyFieldGenerator(2)).toStrictEqual(
+                [
+                    [empty, empty],
+                    [empty, empty]
+                ]
+            )
+        });
+        it('3x3', () => {
+            expect(emptyFieldGenerator(3)).toStrictEqual(
+                [
+                    [empty, empty, empty],
+                    [empty, empty, empty],
+                    [empty, empty, empty],
+                ]
+            )
+        });
+        it('3x3 with hidden state', () => {
+            expect(emptyFieldGenerator(3, hidden)).toStrictEqual(
+                [
+                    [hidden, hidden, hidden],
+                    [hidden, hidden, hidden],
+                    [hidden, hidden, hidden],
+                ]
+            )
+        });
+    });
+
+    describe('Simple field test cases', () => {
+        it('Wrong density', () => {
+            const errorText = 'Probability must be between 0 and 1';
+            expect(() => fieldGenerator(1, -1)).toThrow(errorText);
+            expect(() => fieldGenerator(1, 2)).toThrow(errorText);
+        });
+        it('smallest possible field without mine', () => {
+            expect(fieldGenerator(1, 0)).toStrictEqual([[empty]]);
+        });
+        it('big field without mine', () => {
+            expect(fieldGenerator(10, 0)).toStrictEqual(
+                [
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                    [empty,empty,empty,empty,empty,empty,empty,empty,empty,empty],
+                ]
+            );
+        });
+        it('smallest possible field with mine', () => {
+            expect(fieldGenerator(1, 1)).toStrictEqual([[bomb]]);
+        });
+        it('2x2 field with mine', () => {
+            expect(fieldGenerator(2, 1)).toStrictEqual(
+                [
+                    [bomb, bomb],
+                    [bomb, bomb],
+                ]
+            );
+        });
+        it('2x2 field with 50% probability', () => {
+            const field = fieldGenerator(2, .5);
+            const flatField = field.flat();
+
+            console.table(field);
+            console.table(flatField);
+
+            const cellswithBombs = flatField.filter(cell => cell === bomb)
+            const cellswithoutBombs = flatField.filter(cell => cell === 2)
+
+            expect(cellswithBombs).toHaveLength(2);
+            expect(cellswithoutBombs).toHaveLength(2);
+        });
+        it('Real game field size = 10x10 with 1/4 mined cells (25 mines)', () => {
+            const size = 10;
+            const mines = 25;
+
+            const probability = mines / (size * size);
+            const field = fieldGenerator(size, probability);
+
+            const flatField = field.flat();
+
+            expect(flatField.filter(cellWithBombFilter)).toHaveLength(25);
+          });
+    })
+})
