c60f1d37574f2dbe85d3c4451baab1c369f43228
Sunkanmi-neulogic <sunkanmi.samson@neulogicsolutions.com>
2023-05-22 17:06:39 +0100

    finished grid component and added hooks

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import App from './App'
 import { Top } from "./components/Top/";
+import { Scoreboard} from './components/Scoreboard'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     {/* <App /> */}
-    <Top feature='Flag' firstAction='ctrl' secondAction='click'>
-      Minesweeper
-    </Top>
+    <>
+      <Top feature='Flag' firstAction='ctrl' secondAction='click'>
+        Minesweeper
+      </Top>
+      <Scoreboard
+        time="000"
+        levels={['beginner', 'intermediate', 'expert']}
+        mines="010"
+        onReset={() => null}
+      />
+    </>
   </React.StrictMode>,
 )
